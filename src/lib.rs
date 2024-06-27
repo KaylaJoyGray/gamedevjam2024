c04625d88148ee6c7d1a834ad5ec0d4484980676
Kayla Gray <kjoygray75@gmail.com>
2024-06-26 22:13:03 -0500

    Test

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,22 @@ mod utils;
 mod gfx;
 mod sound;
 
-use bevy::app::App;
-use bevy::DefaultPlugins;
 use wasm_bindgen::prelude::*;
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+mod helpers;
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    let map_handle: Handle<helpers::tiled::TiledMap> = asset_server.load("map.tmx");
+
+    commands.spawn(helpers::tiled::TiledMapBundle {
+        tiled_map: map_handle,
+        ..Default::default()
+    });
+}
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
